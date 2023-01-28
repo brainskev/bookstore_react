c6c283df2c70b43949a165423e578ed099810884
Kelvin <ngotho.kelvin@yahoo.com>
2023-01-29 00:42:25 +0300

    configure linters

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Categories = () => (
-    <div className="categories">
-        <button type="button">Check Status</button>
-    </div>
+  <div className="categories">
+    <button type="button">Check Status</button>
+  </div>
 );
 
 export default Categories;
